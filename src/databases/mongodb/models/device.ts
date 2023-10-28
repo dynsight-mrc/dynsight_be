5bc44fd9d551358042a1d33034ae06a02796e2bd
oussama benkemchi <benkemchioussama@gmail.com>
2023-10-28 16:44:04 +0100

    Perf - update Device model code

--- src/databases/mongodb/models/device.ts
+++ src/databases/mongodb/models/device.ts
@@ -1,18 +1,33 @@
-import mongoose, { Schema } from "mongoose";
-
-const deviceSchema = new Schema({
-  name: { type: String, required: true },
-});
+import mongoose, { Schema, model } from "mongoose";
 
 interface DeviceAttrs {
   name: string;
 }
 
-export interface DeviceDoc extends mongoose.Document {
+interface DeviceDoc extends mongoose.Document {
   name: string;
 }
 
-interface DeviceModel extends mongoose.Model<DeviceDoc> {}
+interface DeviceModel extends mongoose.Model<DeviceDoc> {
+  build(attr: DeviceAttrs): DeviceModel;
+}
+
+const deviceSchema = new Schema(
+  {
+    name: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        (ret.id = doc._id), delete ret._id;
+      },
+    },
+  }
+);
+
+deviceSchema.statics.build = (attrs: DeviceAttrs) => {
+  return new Device(attrs);
+};
 
 const Device = mongoose.model<DeviceDoc, DeviceModel>("Device", deviceSchema);
 
